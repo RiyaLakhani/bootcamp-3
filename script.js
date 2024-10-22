550131205bb85546c0f9c8ca59bb19fcd5b342d2
Riya Lakhani <riyalakhani@wireless-10-105-116-38.umd.edu>
2024-10-22 18:11:36 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let flashcards = [
+    { term: "HTML", definition: "HyperText Markup Language" },
+    { term: "CSS", definition: "Cascading Style Sheets" },
+    { term: "JavaScript", definition: "Programming language of the web" }
+];
+
+
+// You can use flashcards.length to get the length of the array
+
+
+// These two variables will come in handy
+let currentIndex = 0;
+let showingTerm = true;
+
+// Start with this function to simply display the card
+function displayCard() {
+    if (showingTerm) {
+        let term = flashcards[currentIndex].term
+        let cardContent = document.getElementById('card-content')
+        cardContent.innerText = term
+    } else { 
+        let definition = flashcards[currentIndex].definition
+        let cardContent = document.getElementById('card-content')
+        cardContent.innerText = definition
+    }
+}
+
+
+//Change variable and call the function
+
+// The rest of the code you will write is a part of event listeners
+document.getElementById('prev-btn').addEventListener('click', ()=> {
+    currentIndex --;
+    if (currentIndex === -1) {
+        currentIndex = flashcards.length - 1;
+        showingTerm = true;
+    }
+    displayCard();
+})
+
+document.getElementById('next-btn').addEventListener('click', ()=> {
+    currentIndex ++;
+    if (currentIndex === flashcards.length) {
+        currentIndex = flashcards.length;
+    }
+    displayCard();
+})
+
+document.getElementById('flashcard').addEventListener('click', function(){
+    if (showingTerm) {
+        showingTerm = false;
+    } else {
+        showingTerm = true;
+    }
+    displayCard();
+})
+
+document.getElementById('add-card-btn').addEventListener('click', ()=> {
+    let newTerm = document.getElementById('new-term').value;
+    let newDefinition = document.getElementById('new-definition').value;
+    flashcards.push({
+        term: newTerm,
+        definition: newDefinition
+    })
+    document.getElementById('new-term').value = "";
+    document.getElementById('new-definition').value = "";
+})
+
+updateScreen();
+
+// This line will display the card when the page is refreshed
+window.onload = displayCard;
